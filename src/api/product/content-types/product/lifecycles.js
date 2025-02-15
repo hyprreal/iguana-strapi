27155fdb60613702a1d7569a92a0f3b3630ad104
Nathan <nathan@hprl.dev>
2025-02-14 21:56:01 -0500

    refactor: add afterUpdate and afterDelete

--- src/api/product/content-types/product/lifecycles.js
+++ src/api/product/content-types/product/lifecycles.js
@@ -15,12 +15,51 @@ module.exports = {
       if (!response.ok) {
         throw new Error(`Error calling API endpoint: ${response.status} ${response.statusText}`);
       }
-
-      const data = await response.json(); // Process the response from your API endpoint
-      console.log('API call successful:', data);
     } catch (error) {
       console.error('Error calling API endpoint:', error);
       // Handle the error appropriately (e.g., log it, display a message, etc.)
     }
   },
+  async afterUpdate(event) {
+    const { result } = event; // The created entry
+
+    try {
+      const response = await fetch('https://project-iguana.netlify.app/api/product', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          // Add any necessary authentication headers (e.g., Authorization)
+        },
+        body: JSON.stringify(result),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error calling API endpoint: ${response.status} ${response.statusText}`);
+      }
+    } catch (error) {
+      console.error('Error calling API endpoint:', error);
+      // Handle the error appropriately (e.g., log it, display a message, etc.)
+    }
+  },
+  async afterDelete(event) {
+    const { result } = event; // The created entry
+
+    try {
+      const response = await fetch('https://project-iguana.netlify.app/api/product', {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          // Add any necessary authentication headers (e.g., Authorization)
+        },
+        body: JSON.stringify(result),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error calling API endpoint: ${response.status} ${response.statusText}`);
+      }
+    } catch (error) {
+      console.error('Error calling API endpoint:', error);
+      // Handle the error appropriately (e.g., log it, display a message, etc.)
+    }
+  }
 };
