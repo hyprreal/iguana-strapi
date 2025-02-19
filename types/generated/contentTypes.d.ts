896bcfb8c1d12d92b99512a9a28fd1e2c5394ce5
Nathan <nathan@hprl.dev>
2025-02-19 04:13:06 -0500

    refactor: remove photo

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -424,7 +424,6 @@ export interface ApiProductProduct extends Struct.CollectionTypeSchema {
     > &
       Schema.Attribute.Private;
     Name: Schema.Attribute.String & Schema.Attribute.Required;
-    Photo: Schema.Attribute.Media<'images'>;
     Photos: Schema.Attribute.Media<'images' | 'videos', true> &
       Schema.Attribute.Required;
     Price: Schema.Attribute.Decimal;
